af0b7bf001bee260c3e13536339b532d4065de35
Jakub Zegar <jakub.zegar@salesbook.com>
2023-02-06 22:17:24 +0100

    feat: test helper

---
+++ src/TestHelpers/TestHelper.tsx
@@ -0,0 +1,31 @@
+﻿import React, { ReactNode } from "react";
+import { render } from "@testing-library/react";
+import { cardService } from "../services/CardService";
+import { GameContext } from "../context/GameContext";
+import { GameState } from "../const/gameState";
+import { TGameContext } from "../types/global";
+
+export const mockContext: TGameContext = {
+  playerCards: [],
+  croupierCards: [],
+  drawOneCard: () => {},
+  message: "",
+  setMessage: () => {},
+  currentRoundStatus: GameState.DRAW_CARDS,
+  setCurrentRoundStatus: () => {},
+};
+
+const createDeckMock = jest.spyOn(cardService, "createDeck");
+const drawCardsMock = jest.spyOn(cardService, "drawCards");
+
+export const sampleDeckId = "sampleDeckId";
+
+beforeEach(() => {
+  jest.clearAllMocks();
+  createDeckMock.mockResolvedValue(sampleDeckId);
+  drawCardsMock.mockResolvedValue([{ value: "test", image: "", cardId: 0 }]);
+});
+
+export const renderWithContext = (comp: ReactNode, props?: Partial<typeof mockContext>) => {
+  return render(<GameContext.Provider value={{ ...mockContext, ...props }}>{comp}</GameContext.Provider>);
+};
